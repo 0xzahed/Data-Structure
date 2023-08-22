3e4ccfdd6e324e6891ef138237e40f040a4ece32
Abu Zahed <108692668+zahed01x@users.noreply.github.com>
2023-08-23 00:00:57 +0600

    Linked List Creation Insert_End In c

---
+++ insert_end.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+typedef struct node Node;
+
+Node *head = NULL;
+
+void createNode(int n);
+void insertEnd();
+void displayList();
+
+int main() {
+    int n, p;
+    
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+    
+    createNode(n);
+    insertEnd();
+    displayList();
+    
+    return 0;
+}
+
+void createNode(int n) {
+    Node *newNode, *temp;
+    
+    head = (Node *)malloc(sizeof(Node));
+    
+    if (head == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    
+    printf("Enter data for node 1: ");
+    scanf("%d", &head->data);
+    head->next = NULL;
+    temp = head;
+    
+    for (int i = 2; i <= n; i++) {
+        newNode = (Node *)malloc(sizeof(Node));
+        
+        if (newNode == NULL) {
+            printf("Memory allocation failed.\n");
+            exit(1);
+        }
+        
+        printf("Enter data for node %d: ", i);
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+        
+        temp->next = newNode;
+        temp = temp->next;
+    }
+}
+
+void insertEnd() {
+    Node *newNode, *temp = head;
+    
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    
+    newNode = (Node *)malloc(sizeof(Node));
+
+    printf("Enter data for the new node at the end: ");
+    scanf("%d", &newNode->data);
+    newNode->next = NULL;
+    temp->next = newNode;
+}
+
+void displayList() {
+    Node *temp = head;
+    
+    printf("Linked List: ");
+    
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    
+    printf("NULL\n");
+}
